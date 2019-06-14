8aa8276e4aa409ce65bbab134afcf1e369de5226
minkolee <minkolee@gmail.com>
2019-06-14 14:25:06 +0800

    自己编写验证器搞定Vuelidate验证CHECKBOX的问题

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -9,19 +9,17 @@
                             id="email"
                             @blur="$v.email.$touch()"
                             v-model="email">
-                    <p v-if="!$v.email.email">请输入正确的Email地址。</p>
-                    <p v-if="!$v.email.required && $v.email.$dirty">Email地址不能为空。</p>
+                    <p v-if="!$v.email.email">Please provide a valid email address.</p>
+                    <p v-if="!$v.email.required">This field must not be empty.</p>
                 </div>
                 <div class="input" :class="{invalid: $v.age.$error}">
                     <label for="age">Your Age</label>
                     <input
+                            type="number"
                             id="age"
                             @blur="$v.age.$touch()"
-                            v-model="age">
-                    <p v-if="!$v.age.required && $v.age.$dirty">年龄不能为空。</p>
-                    <p v-if="!$v.age.numeric && $v.age.$dirty">必须输入数字。</p>
-                    <p v-if="!$v.age.minValue && $v.age.$dirty &&$v.age.numeric">年龄必须大于{{ $v.age.$params.minValue.min
-                        }}岁。</p>
+                            v-model.number="age">
+                    <p v-if="!$v.age.minVal">You have to be at least {{ $v.age.$params.minVal.min }} years old.</p>
                 </div>
                 <div class="input" :class="{invalid: $v.password.$error}">
                     <label for="password">Password</label>
@@ -53,20 +51,20 @@
                     <button @click="onAddHobby" type="button">Add Hobby</button>
                     <div class="hobby-list">
                         <div
-                                :class="{invalid: $v.hobbyInputs.$each[index].$error}"
                                 class="input"
                                 v-for="(hobbyInput, index) in hobbyInputs"
+                                :class="{invalid: $v.hobbyInputs.$each[index].$error}"
                                 :key="hobbyInput.id">
                             <label :for="hobbyInput.id">Hobby #{{ index }}</label>
                             <input
                                     type="text"
                                     :id="hobbyInput.id"
-                                    @input="$v.hobbyInputs.$each[index].value.$touch()"
+                                    @blur="$v.hobbyInputs.$each[index].value.$touch()"
                                     v-model="hobbyInput.value">
                             <button @click="onDeleteHobby(hobbyInput.id)" type="button">X</button>
                         </div>
-                        <p v-if="!$v.hobbyInputs.minLength || !$v.hobbyInputs.required">You have to select at least {{
-                            $v.hobbyInputs.$params.minLength.min }} hobby(s).</p>
+                        <p v-if="!$v.hobbyInputs.minLen">You have to specify at least {{ $v.hobbyInputs.$params.minLen.min }} hobbies.</p>
+                        <p v-if="!$v.hobbyInputs.required">Please add hobbies.</p>
                     </div>
                 </div>
                 <div class="input inline" :class="{invalid: $v.terms.$invalid}">
@@ -78,7 +76,7 @@
                     <label for="terms">Accept Terms of Use</label>
                 </div>
                 <div class="submit">
-                    <button :disabled="$v.$invalid" type="submit">Submit</button>
+                    <button type="submit" :disabled="$v.$invalid">Submit</button>
                 </div>
             </form>
         </div>
@@ -86,10 +84,11 @@
 </template>
 
 <script>
-    import {required, email, numeric, minValue, minLength, sameAs, requiredUnless} from 'vuelidate/lib/validators';
+    import { required, email, numeric, minValue, minLength, sameAs, requiredUnless } from 'vuelidate/lib/validators'
+    import axios from 'axios'
 
     export default {
-        data() {
+        data () {
             return {
                 email: '',
                 age: null,
@@ -100,84 +99,64 @@
                 terms: false
             }
         },
-
         validations: {
             email: {
                 required,
                 email,
-                myVal: val => {
+                unique: val => {
                     if (val === '') return true
-                    return new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            resolve(val === 'test@test.com')
-                        }, 2000)
-                    })
+                    return axios.get('/users.json?orderBy="email"&equalTo="' + val + '"')
+                        .then(res => {
+                            return Object.keys(res.data).length === 0
+                        })
                 }
             },
             age: {
                 required,
                 numeric,
-                minValue: minValue(18)
+                minVal: minValue(18)
             },
             password: {
                 required,
-                minLength: minLength(4)
+                minLen: minLength(6)
             },
             confirmPassword: {
-                //直接传入字符串
-                // sameAs: sameAs("saner")
-
-                //传入当前实例的属性值
-                sameAs: sameAs(v => v.password)
+//        sameAs: sameAs('password')
+                sameAs: sameAs(vm => {
+                    return vm.password
+                })
             },
             terms: {
-                required: requiredUnless(v => {
-                    return v.country === 'germany'
-                })
+                required: (val,vm)=>{
+                    if ( vm.country === 'germany') {
+                        return true;
+                    }
+                    return vm.terms === true;
+                }
             },
             hobbyInputs: {
                 required,
-                minLength: minLength(2),
-                //$each表示当前array中的每一个元素
-                //所以其中要对这个元素的属性进行验证
-                //$each代表的是hobbyInput对象，要验证其value，验证器中的属性就是value
+                minLen: minLength(2),
                 $each: {
                     value: {
                         required,
-                        minLength: minLength(5)
+                        minLen: minLength(5)
                     }
                 }
             }
         },
-
-        computed: {
-            errorStyle: function () {
-                if (!this.$v.email.$dirty) {
-                    console.log(this.$v.email.$dirty)
-                    return false;
-                } else {
-                    console.log(this.$v.email.$error)
-                    if (this.$v.email.$error) {
-                        return true
-                    } else {
-                    }
-                    return false;
-                }
-            }
-        },
-
         methods: {
-            onAddHobby() {
+            onAddHobby () {
                 const newHobby = {
                     id: Math.random() * Math.random() * 1000,
                     value: ''
                 }
                 this.hobbyInputs.push(newHobby)
             },
-            onDeleteHobby(id) {
+            onDeleteHobby (id) {
                 this.hobbyInputs = this.hobbyInputs.filter(hobby => hobby.id !== id)
             },
-            onSubmit() {
+            onSubmit () {
                 const formData = {
                     email: this.email,
                     age: this.age,
@@ -186,26 +165,9 @@
                     country: this.country,
                     hobbies: this.hobbyInputs.map(hobby => hobby.value),
                     terms: this.terms
-                };
-                console.log("Email is " + formData.email)
-                console.log("Email is " + formData.password)
-
-                // axios.post('/signupNewUser?key=AIzaSyAtwQ-RL7GKu0T1h4kOy32JRXmbmQPqiHM', {
-                //   email: formData.email,
-                //   password: formData.password,
-                //   returnSecureToken: true
-                // })
-                //         .then(response => {
-                //           console.log(response);
-                //         })
-                //         .catch(error=> {
-                //           console.log(error);
-                //         });
-                this.$store.dispatch('signup', {
-                    email: formData.email,
-                    password: formData.password,
-                    returnSecureToken: true
-                }).then(() => console.log("action-signup执行完成"));
+                }
+                console.log(formData)
+                this.$store.dispatch('signup', formData)
             }
         }
     }
@@ -222,15 +184,6 @@
 
     .input {
         margin: 10px auto;
-    }
-
-    .input.invalid label {
-        color: red;
-    }
-
-    .input.invalid input {
-        border: 1px solid red;
-        background-color: #ffc9aa;
     }
 
     .input label {
@@ -259,6 +212,15 @@
         outline: none;
         border: 1px solid #521751;
         background-color: #eee;
+    }
+
+    .input.invalid label {
+        color: red;
+    }
+
+    .input.invalid input {
+        border: 1px solid red;
+        background-color: #ffc9aa;
     }
 
     .input select {
